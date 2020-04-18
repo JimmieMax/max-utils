7ec6fa98ba0279f0b509d83f73001bb8ce94c2a9
Jimmie Max <jimmiemaxgo@gmail.com>
2020-04-18 10:38:40 +0800

    feat[add changelog]

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+## Changelog
+##### 1.0.3 2020.04.18
+- Improvements of the `postinstall` script
+
+##### 0.1.0 - 2020.01.01
+* Public release
--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -15,9 +15,6 @@ function log(it) {
 }
 
 if (!ADBLOCK && !CI && !SILENT) {
-  log('\u001B[96mThank you for using max-utils (\u001B[94m https://github.com/JimmieMax/max-utils \u001B[0m\n');
-  // log('\u001B[96mThe project needs your help! Please consider supporting of core-js on Open Collective or Patreon: \u001B[0m');
-  // log('\u001B[96m>\u001B[94m https://opencollective.com/core-js \u001B[0m');
-  // log('\u001B[96m>\u001B[94m https://www.patreon.com/zloirock \u001B[0m\n');
-  // log('\u001B[96mAlso, the author of core-js (\u001B[94m https://github.com/zloirock \u001B[96m) is looking for a good job -)\u001B[0m\n');
+  log('\u001B[96mThank you for using max-utils (\u001B[94m https://github.com/JimmieMax/max-utils \u001B[96m)');
+  log('\u001B[96mIf you like it, Please Give me a star! \u001B[0m');
 }
